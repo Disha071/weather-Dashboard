9d82b7bec1077002d2c59a21adccb38025eaf162
Disha Gupta <dishaguptaa77@gmail.com>
2024-07-18 15:53:44 +0530

    Initial commit

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "8be0628fb5msh6fffb2409e7499bp1c7188jsn2fd95872da0b",
+      "X-RapidAPI-Key": "c9efa81b8dmsh149823994cb769dp191dd6jsna3c4a2d39cf3",
       "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
     },
   };
   export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
   
   export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-  export const WEATHER_API_KEY = "18763be5923a94cc59f5d40f3742ecff";
+  export const WEATHER_API_KEY = "95c6b79190d94d40d1dc83e580526dda";
